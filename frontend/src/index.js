e256e4e4b3edcf4e84fe37ba9b6ed38ea31f8437
Dylan Longlett <dnlonglett@gmail.com>
2026-02-26 17:13:21 -0600

    Set default body styles to remove margin and match app background

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Remove default body margin and match app background to avoid white border
+document.body.style.margin = '0';
+document.body.style.background = '#181c20';
+document.body.style.color = '#f5f6fa';
 root.render(
   <React.StrictMode>
     <App />
