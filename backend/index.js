16012042e442211978013219bcb0e4fdf14ed8d7
Dylan Longlett <dnlonglett@gmail.com>
2026-02-26 22:40:52 -0600

    Update PORT configuration to default to 5000 in index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import path from 'path';
 import bcrypt from 'bcrypt';
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/caltrack', {
