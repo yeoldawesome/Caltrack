b92b1f645b7c15aab9ebbd9559f06a3f6f1dfac8
Dylan Longlett <dnlonglett@gmail.com>
2026-02-26 21:48:57 -0600

    Refactor dotenv import and simplify MongoDB connection options

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
@@ -15,7 +14,7 @@ import mongoose from 'mongoose';
 
 // MongoDB Atlas connection
 const mongoUri = process.env.MONGO_URI;
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUri)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
