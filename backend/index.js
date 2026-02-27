4f8115ff30ef03c584eeac747f77adaf970ea6ac
Dylan Longlett <dnlonglett@gmail.com>
2026-02-26 21:37:05 -0600

    Refactor dotenv import and restore todo list

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-// Trust proxy for correct secure cookie handling on Render
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
