28220c369bee7b72e8945f78f19fb9cd36903b8c
Dylan Longlett <dnlonglett@gmail.com>
2026-02-26 22:52:45 -0600

    Remove local MongoDB connection from index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/caltrack');
 const sessionSchema = new mongoose.Schema({
   date: String,
   calories: Number,
